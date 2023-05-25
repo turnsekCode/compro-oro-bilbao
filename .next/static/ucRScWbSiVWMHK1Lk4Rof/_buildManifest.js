2480a01188b07acbc1908c17feb033a9571bf98b
abraham <abraham.turnsek@gmail.com>
2023-05-25 15:22:31 +0200

    version 1.0

---
+++ .next/static/ucRScWbSiVWMHK1Lk4Rof/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/336-d7fa709dad427dd3.js","static/css/e310174a932f1668.css","static/chunks/pages/index-ad6ad8efc0cbd191.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
